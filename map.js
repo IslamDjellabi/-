b027d4ffaaf56db1e28dbed1807d38d8e08b669b
IslamDjellabi <islam59k@gmail.com>
2025-09-11 12:18:46 +0100

    Add files via upload

---
+++ map.js
@@ -0,0 +1,35 @@
+
+function openFullscreen() {
+      let elem = document.getElementById("map-container");
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    }
+
+
+
+
+    const btn = document.getElementById('fullscreen-btn');
+const img = document.getElementById('logo-img');
+
+btn.addEventListener('click', () => {
+  if (!document.fullscreenElement) {
+    img.requestFullscreen();
+    btn.textContent = 'Exit Fullscreen';
+  } else {
+    document.exitFullscreen();
+    btn.textContent = 'Fullscreen';
+  }
+});
+
+document.addEventListener('fullscreenchange', () => {
+  if (!document.fullscreenElement) {
+    btn.textContent = 'Fullscreen';
+  }
+});
