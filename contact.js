64def2db60b25399b44f8725df34eb8153e40fa6
IslamDjellabi <islam59k@gmail.com>
2025-10-27 16:52:36 +0100

    Add files via upload

--- contact.js
+++ contact.js
@@ -46,3 +46,43 @@ function showToast(message) {
 }
 
 
+
+
+
+// تفعيل EmailJS
+emailjs.init("ySqctbW0iXg4_IF7i"); // استبدلها بمفتاحك من EmailJS
+
+const form = document.getElementById("support-form");
+const statusMsg = document.getElementById("status-message");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
+  };
+
+  emailjs.send("service_6ccdzsr", "template_rdwbfro", formData)
+   .then(() => {
+  statusMsg.textContent = "✅ تم إرسال رسالتك بنجاح";
+  statusMsg.className = "success";
+  statusMsg.classList.remove("hidden");
+  form.reset();
+}, (error) => {
+  statusMsg.textContent = "❌ حدث خطأ أثناء الإرسال، حاول مجددًا.";
+  statusMsg.className = "error";
+  statusMsg.classList.remove("hidden");
+  console.error("EmailJS Error:", error);
+});
+});
+
+/* 🔹 خاصية رفع النموذج عند ظهور لوحة المفاتيح على الهاتف */
+window.addEventListener("resize", () => {
+  if (window.innerHeight < 500) {
+    document.querySelector(".support-section").style.justifyContent = "flex-start";
+  } else {
+    document.querySelector(".support-section").style.justifyContent = "center";
+  }
+});
